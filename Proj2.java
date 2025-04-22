16abd2f265a30a333128a405c9da40a16c6048bf
AmirhosseinMoslehi <amoslehi84@gmail.com>
2025-04-22 08:28:55 +0330

    Add files via upload

---
+++ Proj2.java
@@ -0,0 +1,9 @@
+public class main {
+  public static void main(String[] args)
+  {
+    System.out.println("First name : Amir hossein");
+    System.out.println("Last name : Moslehi");
+    System.out.println("Student ID : 03111142302024");
+    System.out.println("Course : mobile programming 1");
+  }
+}
